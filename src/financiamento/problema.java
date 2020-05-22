0fc12b0a67a5c31f2c36a7bb0c3bbeb69ab1d9d5
Igor Machado <igorhms98@gmail.com>
2020-05-22 20:39:13 -0300

    Leitura do nome e salario

--- src/financiamento/problema.java
+++ src/financiamento/problema.java
@@ -1,10 +1,31 @@
 package financiamento;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class problema {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String nome;
+		double salario;
+		
+		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
+		System.out.print("Nome do cliente: ");
+		nome = sc.nextLine();
+		System.out.print("Salario: ");
+		salario = sc.nextDouble();
+		while (salario < 0){
+			System.out.print("Salario: ");
+			salario = sc.nextDouble();
+		}
+		
+		
+		
+		sc.close();
 	}
+	
 
 }
